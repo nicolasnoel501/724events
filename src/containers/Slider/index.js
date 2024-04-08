a306c8f1f3eafa2407989a2ccea625a0f0ac6d7e
Nicolas Noël <nicolasnoel501@gmail.com>
2024-04-08 14:45:06 +0200

    correction of the slider, the slides rebound well without display error

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,57 +1,52 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
-
 import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  const sortedEvents = data?.focus.sort((evtA, evtB) =>
+    new Date(evtA.date) - new Date(evtB.date)
   );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
+
   useEffect(() => {
-    nextCard();
-  });
+    const intervalId = setInterval(() => {
+      setIndex((prevIndex) =>
+        prevIndex < sortedEvents.length - 1 ? prevIndex + 1 : 0
+      );
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [sortedEvents]);
+
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+      {sortedEvents?.map((event, idx) => (
+        <div key={event.id} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {sortedEvents.map((event) => (
+            <input
+              key={event.id}
+              type="radio"
+              readOnly
+              name="radio-button"
+              checked={index === sortedEvents.indexOf(event)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
