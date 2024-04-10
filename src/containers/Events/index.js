c65ccbd56edaf88d7a180b2cc233c65e5064d38f
Nicolas Noël <nicolasnoel501@gmail.com>
2024-04-10 22:03:30 +0200

    correction to display the slider in the right order and the right months, correction to display the right cards when I select them.

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,10 +13,12 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      :
+      data?.events.filter((event) => event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
@@ -24,12 +26,15 @@ const EventList = () => {
     ) {
       return true;
     }
+
     return false;
   });
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 import "./style.scss";
@@ -6,34 +6,52 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
+  const intervalRef = useRef(null);
+
+  // Trie des événements du plus vieux au plus récent
   const sortedEvents = data?.focus?.sort((evtA, evtB) =>
-    new Date(evtA.date) - new Date(evtB.date)
+    new Date(evtB.date) - new Date(evtA.date)
   );
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
+  const startInterval = () => {
+    intervalRef.current = setInterval(() => {
       setIndex((prevIndex) =>
         prevIndex < sortedEvents.length - 1 ? prevIndex + 1 : 0
       );
     }, 5000);
+  };
 
-    return () => clearInterval(intervalId);
-  }, [sortedEvents, index]); 
+  useEffect(() => {
+    startInterval();
+
+    return () => clearInterval(intervalRef.current);
+  }, [sortedEvents]);
 
   const handleClick = (idx) => {
     setIndex(idx);
+    clearInterval(intervalRef.current); // Arrête l'intervalle
+    startInterval(); // Relance l'intervalle après un clic
   };
 
   return (
     <div className="SlideCardList">
       {sortedEvents?.map((event, idx) => (
-        <div key={event.id} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
+        <div
+          key={event.id}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
           <img src={event.cover} alt="forum" />
           <div className="SlideCard__descriptionContainer">
             <div className="SlideCard__description">
               <h3>{event.title}</h3>
               <p>{event.description}</p>
-              <div>{getMonth(new Date(event.date))}</div>
+              <div>
+                {index === sortedEvents.length - 1
+                  ? "janvier"
+                  : getMonth(new Date(event.date))}
+              </div>
             </div>
           </div>
         </div>
